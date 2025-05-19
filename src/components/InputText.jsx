119b41df5741b949cb9b48282bc0aaa49dba38bf
ETML-Midicix <pa64wln@eduvaud.ch>
2025-05-19 10:19:44 +0200

    [fix] reset room value when pressing ENTER

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -15,7 +15,8 @@ export default ({id, value, setter, placeholder, exportedValiditySetter, type="t
     }
   },[value])
 
-  const handleDeleteEntry = () =>{
+  const handleDeleteEntry = () => {
+    console.log("efesf")
     setter("");
     setIsValid(false);
   }
@@ -39,9 +40,14 @@ export default ({id, value, setter, placeholder, exportedValiditySetter, type="t
         htmlFor={idInput}
         className={`cursor-text select-none absolute text-sm ${labelColorClassName ? labelColorClassName : `peer-focus:text-[#B51F1F] ${pattern ? isValid ? "text-[#FF0000]" : "text-gray-500" : value ? "text-[#FF0000]" : "text-gray-500"}`} duration-200 transform -translate-y-4 scale-75 top-1.5 left-3 z-10 origin-[0] bg-[#EEEEEE] px-2 peer-placeholder-shown:text-xl peer-focus:text-sm peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1.5 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1`}>{placeholder}</label>
       </div>
-      <svg className={`h-5 w-5 absolute top-3.5 right-3.5 text-gray-400 ${value ? "" : "hidden"} animate-[ping_.2s_ease-in-out_forwards_reverse] hover:cursor-pointer`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" onClick={()=>handleDeleteEntry()} tabIndex={0} onKeyDown={(e)=> {closingHandler(e) ? (handleDeleteEntry()) : ("")}}>
+      {/* <svg id={`${id}-remover`} className={`h-5 w-5 absolute top-3.5 right-3.5 text-gray-400 ${value ? "" : "hidden"} animate-[ping_.2s_ease-in-out_forwards_reverse] hover:cursor-pointer`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" onClick={()=>handleDeleteEntry()} tabIndex={0} onKeyDown={(e)=> {closingHandler(e) ? (handleDeleteEntry()) : ("")}}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-      </svg>
+      </svg> */}
+      <button type="button" id={`${id}-remover`} className={`h-5 w-5 absolute top-3.5 right-3 ${value ? "" : "hidden"}`} onClick={()=>handleDeleteEntry()} tabIndex={0} onKeyDown={(e)=> {closingHandler(e) ? (handleDeleteEntry()) : ("")}}>
+        <svg className={`text-gray-400 ${value ? "" : "hidden"} animate-[ping_.2s_ease-in-out_forwards_reverse] hover:cursor-pointer`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+      </button>
     </div>
   );
 };
