f3248bf718ef306147999286a73ef71bbf850b9e
FlizzerMDX <pa64wln@eduvaud.ch>
2025-09-29 15:15:08 +0200

    [docs] add select parameters
    also fix translation error in french due to refactor from ressource to resource, tags write wrong in fr.json

--- src/components/docs/MiddleContent/endpoints/DocsParamsExplanation.jsx
+++ src/components/docs/MiddleContent/endpoints/DocsParamsExplanation.jsx
@@ -11,20 +11,31 @@ export default ({ params, translationHandler, paramsTranslationPath, color }) =>
                     {
                         type: params.type,
                         format: params.format,
-                        name: <span className="font-bold" key={params.key}>{ params.name }</span>,
-                        acceptedSynthax:params.acceptedSynthax.map(
+                        name: params.name,
+                        isRequired: params.required,
+                        acceptedSynthax: params.acceptedSynthax ? params.acceptedSynthax.map(
                             (synthax, index) =>
                                 <span key={synthax}><span style={{ '--custom-bg-color': color }} className="border border-[var(--custom-bg-color)] p-[2px] rounded-lg bg-[var(--custom-bg-color)] bg-opacity-50">{synthax}</span><span>{index == params.acceptedSynthax.length - 1 || params.acceptedSynthax.length <= 1 ?
                                 "" :
                                 index == params.acceptedSynthax.length - 2 ?
                                     " and " :
                                     ", "
-                            }</span></span>
-                        )
+                                }</span></span>
+                            )
+                            : ""
                     }
                 )
                 }
             </p>
+            <ul className="list-disc ml-8 my-4">
+                {
+                    params.keyArray ? params.keyArray.map(p =>
+                        <li key={p}>
+                            {p} ({translationHandler(`${translationPath}.keyArrayExplanation.${p}`)})
+                        </li>
+                    ) : <></>
+                }
+            </ul>
         </div>
     );
 };
