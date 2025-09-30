3aac367c1d9bf1849eabacdbefe45f0f64a6c146
FlizzerMDX <pa64wln@eduvaud.ch>
2025-09-30 10:16:17 +0200

    [feature] add reset button for console in documentation
    to remove lags when is there a lot of data

--- src/components/docs/MiddleContent/endpoints/DocsDetails.jsx
+++ src/components/docs/MiddleContent/endpoints/DocsDetails.jsx
@@ -35,8 +35,8 @@ export default ({ endpoint, ep, rootTranslationId, endpointTranslationPath }) =>
 
     const translationHandler = useTranslations(rootTranslationId);
 
-
-    const [code, setCode] = useState(`{"message" : "${translationHandler("return.placeholder")}"}`);
+    const initCode = `{"message" : "${translationHandler("return.placeholder")}"}`;
+    const [code, setCode] = useState(initCode);
     const [isLoading, setIsLoading] = useState(false);
     const [params, setParams] = useState([]);
     const [endpointUrl, setEndpointUrl] = useState("/export");
@@ -181,8 +181,8 @@ export default ({ endpoint, ep, rootTranslationId, endpointTranslationPath }) =>
                             </div>
                             <div className="m-2">
                                 <CodeBlock id={`${id}${separatorId}console`} code={code} language="json" />
-                                <div className="w-full text-center m-2">
-                                    <button className="bg-[#4990e2] p-1 rounded-md text-white font-bold hover:cursor-pointer flex mx-auto"
+                                <div className="w-full text-center m-2 flex justify-center mx-auto">
+                                    <button className="bg-[#4990e2] p-1 rounded-md text-white font-bold hover:cursor-pointer flex mx-2"
                                         onClick={() => { setIsLoading(true); callAPI(endpointUrl, setCode, setIsLoading); }}>
                                         {
                                             isLoading ?
@@ -194,7 +194,14 @@ export default ({ endpoint, ep, rootTranslationId, endpointTranslationPath }) =>
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                                 </svg>
                                         }
-                                        <label className="hover:cursor-pointer">{translationHandler("console.button.label")}</label>
+                                        <label className="hover:cursor-pointer">{translationHandler("console.button.launch.label")}</label>
+                                    </button>
+                                    <button className="bg-[#B51F1F] p-1 rounded-md text-white font-bold hover:cursor-pointer flex mx-2"
+                                        onClick={() => { setCode(initCode); }}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                        <label className="hover:cursor-pointer">{translationHandler("console.button.reset.label")}</label>
                                     </button>
                                 </div>
                             </div>
