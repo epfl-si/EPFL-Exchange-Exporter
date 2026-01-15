27d633f0a29f4d432c75dceaed80be37858ce018
FlizzerMDX <pa64wln@eduvaud.ch>
2026-01-15 15:33:17 +0100

    [feature] add 'InvalidAuthenticationToken' error in documentation

--- src/class/APIReturnClass.js
+++ src/class/APIReturnClass.js
@@ -7,6 +7,7 @@ export default class {
   constructor(data, header){
     if (data.error) {
       // this.status = infoKeywordList.includes(data.error.code) ? "info" : warningKeywordList.includes(data.error.code) ? "warning" : "info";
+      console.log(data.error)
       this.status = docs.errors.filter(x => x.code == data.error.code)[0].status;
       this.error = {
         ...data.error,
